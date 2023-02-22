69e4a7bfc9a5c9918080e5c1de3c0cc5759bc0ee
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-22 13:54:31 +0100

    a03: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A035F Script version: 3.0.0

---
+++ omni_a03.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a03 device
+$(call inherit-product, device/samsung/a03/device.mk)
+
+PRODUCT_DEVICE := a03
+PRODUCT_NAME := omni_a03
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A035F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a03nnxx-user 11 RP1A.200720.012 A035FXXU2CWA5 release-keys"
+
+BUILD_FINGERPRINT := samsung/a03nnxx/a03:11/RP1A.200720.012/A035FXXU2CWA5:user/release-keys
